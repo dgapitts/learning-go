2039b4aa1a65f95ee2f291cc76092db363c09b6f
Dave Pitts <dgapitts@gmail.com>
2022-01-07 21:02:57 +0100

    ex004_go_math exploring maths, bigints and factorial functions in go

---
+++ ex004_go_math/factorial.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Please enter number: ")
+	input1, _ := reader.ReadString('\n')
+	fmt.Println("Got it, you entered : ", input1)
+	int1, _ := strconv.ParseInt(strings.TrimSpace(input1), 0, 64)
+	fmt.Println(factorial(big.NewInt(int1)))
+}
+
+func factorial(x *big.Int) *big.Int {
+	n := big.NewInt(1)
+	if x.Cmp(big.NewInt(0)) == 0 {
+		return n
+	}
+	return n.Mul(x, factorial(n.Sub(x, n)))
+}
