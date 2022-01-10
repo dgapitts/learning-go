6be16bffcabf4159efcf7dc36c011a440a20b10c
Dave Pitts <dgapitts@gmail.com>
2022-01-10 21:36:47 +0100

    ex004_time_pkg more sample formats

--- ex004_time_pkg/time.go
+++ ex004_time_pkg/time.go
@@ -1,6 +1,3 @@
-// https://gobyexample.com/time-formatting-parsing
-
-
 package main
 
 import (
@@ -9,30 +6,20 @@ import (
 )
 
 func main() {
-    p := fmt.Println
 
+    // https://gobyexample.com/time-formatting-parsing
+    p := fmt.Println
     t := time.Now()
     p(t.Format(time.RFC3339))
 
-    t1, e := time.Parse(
-        time.RFC3339,
-        "2012-11-01T22:08:41+00:00")
-    p(t1)
+    // https://www.geeksforgeeks.org/time-formatting-in-golang/
+    current_time := time.Now()
+    fmt.Println("ANSIC: ", current_time.Format(time.ANSIC))
+    fmt.Println("UnixDate: ", current_time.Format(time.UnixDate))
+    fmt.Println("RFC1123: ", current_time.Format(time.RFC1123))
+    fmt.Println("RFC3339Nano: ", current_time.Format(time.RFC3339Nano))
+    fmt.Println("RubyDate: ", current_time.Format(time.RubyDate))
 
-    p(t.Format("3:04PM"))
-    p(t.Format("Mon Jan _2 15:04:05 2006"))
-    p(t.Format("2006-01-02T15:04:05.999999-07:00"))
-    form := "3 04 PM"
-    t2, e := time.Parse(form, "8 41 PM")
-    p(t2)
-
-    fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
-        t.Year(), t.Month(), t.Day(),
-        t.Hour(), t.Minute(), t.Second())
-
-    ansic := "Mon Jan _2 15:04:05 2006"
-    _, e = time.Parse(ansic, "8:41PM")
-    p(e)
 }
 
 
